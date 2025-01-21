5aecf58cac17e2fc3150b5496206ae65566e10c4
amoora <omardiab.m@gmail.com>
2025-01-20 21:54:52 -0500

    spiderman2

--- Lab3A.py
+++ Lab3A.py
@@ -1,6 +1,6 @@
 currentBalance = float(input("Amount owed: $"))
 apr = float(input("APR: "))
-mpr = round(apr / 12,3)
+mpr = apr / 12
 print(f"Monthly percentage rate: {mpr:.3f}")
 minPay = (currentBalance * mpr)/100
 print(f"Minimum payment: $ {minPay:.2f}")
